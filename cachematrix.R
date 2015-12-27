2a6e0ab236ce9f02045cd079060d1e86efdc60f9
Mike Rickher <mikat@cox.net>
2015-12-27 06:33:38 -0700

    initial create of matrix cache and solve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache for a Vector and Its Inversion
+## Adapted from `Caching the Mean of a Vector - rdpeng`
 
-## Write a short comment describing this function
+## Uses R scoping to create an environment that caches a matrix and its 
+## inversion. The inverted matrix is initially NULL indicating it has not been 
+## calculated. After the inversion for the matrix has been calculated it is 
+## stored and reused from the cache avoiding the overhead of recalculation. When
+## the matrix in the cache is changed, the inverted matrix is reset to NULL so
+## it will cause calculation on the next request for the invert.
 
-makeCacheMatrix <- function(x = matrix()) {
+## Usage:
+##
+## mc <- makeCacheMatrix(m1)  run 1st, sets up a cache with an initial matrix 
+## 
+## The output from makeCacheMatrix is the argument for cacheSolve
+## 
+## im <- cacheSolve(mc)  returns the inverse 
+## 
+## mc$setmatrix(m2)  change the matrix in the cache
 
+
+## makeCacheMatrix: Set up a cache for a matrix and its inversion and store the
+## initial matrix in the cache. Returns a list of named functions
+
+makeCacheMatrix <- function(cached_matrix = matrix()) {
+    cached_invert <- NULL
+    
+    setmatrix <- function(new_matrix) {
+        cached_matrix <<- new_matrix
+        cached_invert <<- NULL
+    }
+    getmatrix <- function() {
+        cached_matrix
+    }
+    setinvert <- function(invert) {
+        cached_invert <<- invert
+    }
+    getinvert <- function() {
+        cached_invert
+    }
+    list(setmatrix = setmatrix, 
+         getmatrix = getmatrix,
+         setinvert = setinvert,
+         getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: Returns the inverse of the matrix in the cached environment
+## associated to the `mc` argument. If the matrix in the cache is new or
+## changed, the inverse is calculated. Otherwise the cached version of the
+## inverse is used.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mc, ...) {
+
+    inverted <- mc$getinvert()
+    if(!is.null(inverted)) {
+        message("getting cached data")
+        return(inverted)
+    }
+    data <- mc$getmatrix()
+    inverted <- solve(data, ...)
+    mc$setinvert(inverted)
+    inverted
 }
